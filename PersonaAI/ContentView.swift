6a839e638467980c53bf53f7225b788bc47ca5a0
Heical Chandra <haical31@gmail.com>
2024-05-28 16:35:40 +0700

    update v1

--- PersonaAI/ContentView.swift
+++ PersonaAI/ContentView.swift
@@ -8,14 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var textInput = ""
+    @State var logoAnimating = false
+    @State var timer:Timer?
+    @State var chatService = ChatService()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            ScrollViewReader(content: { proxy in
+                ScrollView{
+                    ForEach(chatService.messages){ chatMessage in
+                        //chatview
+                        chatMessageView(chatMessage)
+                    }
+                }
+                .onChange(of: chatService.messages){ _, _ in
+                    guard let recentMessage = chatService.messages.last else{return}
+                    DispatchQueue.main.async {
+                        withAnimation{
+                            proxy.scrollTo(recentMessage.id, anchor: .bottom)
+                        }
+                    }
+                }
+                .onChange(of: chatService.loadingResponse){_, newValue in
+                    if newValue {
+                        startLoadingAnimation()
+                    }else {
+                        stopLoadingAnimation()
+                    }
+                }
+            })
+            //input fields
+            HStack{
+                TextField("Enter a message...", text: $textInput)
+                    .textFieldStyle(.roundedBorder)
+                    .foregroundColor(.black)
+                Button(action: sendMessage){
+                    Image(systemName: "paperplane.fill")
+                }
+            }
         }
+        .foregroundStyle(.white)
         .padding()
+        .background{
+            ZStack{
+                Color.black
+            }
+            .ignoresSafeArea()
+        }
+    }
+    //chat message view
+    @ViewBuilder func chatMessageView(_ message: ChatMessage) -> some View {
+        ChatBubble(direction: message.role == .model ? .left : .right){
+            Text(message.message)
+                .font(.title3)
+                .padding(.all, 20)
+                .foregroundStyle(.white)
+                .background(message.role == .model ? Color.blue : Color.green)
+        }
+    }
+    //fetch response
+    func sendMessage(){
+        chatService.sendMessage(textInput)
+        textInput = ""
+    }
+    
+    //response loading animation
+    func startLoadingAnimation(){
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: {timer in
+            logoAnimating.toggle()
+        })
+    }
+    func stopLoadingAnimation(){
+        logoAnimating = false
+        timer?.invalidate()
+        timer = nil
     }
 }
 
---
+++ PersonaAI/Utils/ChatService.swift
@@ -0,0 +1,58 @@
+//
+//  ChatService.swift
+//  PersonaAI
+//
+//  Created by Heical Chandra on 28/05/24.
+//
+
+import Foundation
+import SwiftUI
+import GoogleGenerativeAI
+
+enum ChatRole {
+    case user
+    case model
+}
+
+struct ChatMessage: Identifiable, Equatable {
+    let id = UUID().uuidString
+    var role: ChatRole
+    var message: String
+}
+
+@Observable
+class ChatService {
+    private var chat: Chat?
+    private(set) var messages = [ChatMessage]()
+    private(set) var loadingResponse = false
+    
+    func sendMessage(_ message: String){
+        loadingResponse = true
+        
+        if (chat == nil){
+            let history: [ModelContent] = messages.map { ModelContent(role: $0.role == .user ? "user" : "model", parts: $0.message)}
+            chat = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default).startChat(history: history)
+        }
+        
+        //add user message to list
+        messages.append(.init(role: .user, message: message))
+        
+        Task {
+            do {
+                let response = try await chat?.sendMessage(message)
+                
+                loadingResponse = false
+                
+                guard let text = response?.text else {
+                    messages.append(.init(role: .model, message: "Something went wrong, please try again 111"))
+                    return
+                }
+                
+                messages.append(.init(role: .model, message: text))
+            } catch {
+                loadingResponse = false
+                messages.append(.init(role: .model, message: "Something went wrong, please try again 222"))
+            }
+        }
+    }
+}
