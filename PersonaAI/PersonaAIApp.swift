f47e802517f206beefd2bff466f354cdb0093817
Heical Chandra <haical31@gmail.com>
2024-05-28 15:29:38 +0700

    Initial commit

---
+++ PersonaAI/PersonaAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonaAIApp.swift
+//  PersonaAI
+//
+//  Created by Heical Chandra on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonaAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
